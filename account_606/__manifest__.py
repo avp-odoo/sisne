ba5f3fa0311d5b39d896cbf1611ac2b08a76226e
Avadhesh Patel <avp@avp.odoo.co.in>
2018-05-01 12:39:26 +0530

    [ADD] Added module base_vat_do and account_606

---
+++ account_606/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Account extended',
+    'version': '1.0',
+    'category': 'Accounting',
+    'description': """
+    """,
+    'depends': ['account'],
+    'website': 'https://www.odoo.com/page/accounting',
+    'data': [
+        'views/account_invoice.xml',
+        'views/account_tax.xml',
+    ],
+}
